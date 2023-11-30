8369071ac4ff33fe11117d92270464c1e7fedeec
saimomislam26 <saimomislam16@gmail.com>
2023-12-01 01:58:41 +0600

    banner campaign test cases checked

--- src/components/campaign-banner/CampaignBanner.test.tsx
+++ src/components/campaign-banner/CampaignBanner.test.tsx
@@ -1,18 +1,78 @@
+import '@testing-library/jest-dom/extend-expect';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { CampaignBanner } from './CampaignBanner';
+import { gameConfig } from '../../config/game-config';
+import { TestElement } from '../../enums/TestElement';
 
 afterEach(cleanup);
 
 describe('[GAME] CampaignBanner', () => {
   it('SHOULD Render CampaignBanner with all the CSS properties from config file', () => {
     //
+    render(<CampaignBanner />);
+
+    // Assert the existence of BannerStyled
+    expect(screen.getByTestId(TestElement.CAMPAIGN_BANNER)).toBeInTheDocument();
+
+    // Assert the CSS properties from the config file
+    expect(screen.getByTestId(TestElement.CAMPAIGN_BANNER)).toHaveStyle({
+      top: gameConfig.campaignBanner.offsetTop,
+      width: gameConfig.campaignBanner.width,
+      borderRadius: `${gameConfig.campaignBanner.borderRadius}px`,
+      padding: `${gameConfig.campaignBanner.padding}px`,
+      backgroundColor: gameConfig.campaignBanner.background,
+      color: gameConfig.campaignBanner.textColor,
+    });
   });
 
   it('SHOULD Render CampaignBanner and maintain responsiveness WHEN viewport width is 250 px', () => {
     //
+    global.innerWidth = 250;
+
+    render(<CampaignBanner />);
+    // Assert data-testid attribute
+    expect(screen.getByTestId(TestElement.CAMPAIGN_BANNER)).toHaveAttribute('data-testid', TestElement.CAMPAIGN_BANNER);
+
+    const bannerStyle = screen.getByTestId(TestElement.CAMPAIGN_BANNER)
+    // Assuming proportional adjustment
+    const expectedBannerStyles = {
+      top: `${gameConfig.campaignBanner.offsetTop}`, 
+      width: `${gameConfig.campaignBanner.width}`, 
+      borderRadius:`${gameConfig.campaignBanner.borderRadius}px`,
+      padding: `${gameConfig.campaignBanner.padding}px`,
+      backgroundColor: `${gameConfig.campaignBanner.background}`,
+      color: `${gameConfig.campaignBanner.textColor}`,
+    };
+
+    // Assert styles
+    expect(bannerStyle).toHaveStyle('top: ' + `${expectedBannerStyles.top}`);
+    expect(bannerStyle).toHaveStyle('width: ' + `${expectedBannerStyles.width}`);
+    expect(bannerStyle).toHaveStyle('border-radius: ' + `${expectedBannerStyles.borderRadius}`);
+    expect(bannerStyle).toHaveStyle('padding: ' + `${expectedBannerStyles.padding}`);
+    expect(bannerStyle).toHaveStyle('background-color: ' + `${expectedBannerStyles.backgroundColor}`);
+    expect(bannerStyle).toHaveStyle('color: ' + `${expectedBannerStyles.color}`);
+
   });
 
   it('SHOULD Render CampaignBanner and maintain responsiveness WHEN when viewport height is 600 px', () => {
     //
+    window.innerHeight = 600;
+    window.dispatchEvent(new Event('resize'));
+
+    render(<CampaignBanner />);
+
+    const campaignBanner = screen.getByTestId(TestElement.CAMPAIGN_BANNER);
+
+    const expectedTop = `${Math.ceil((600 / 640) * parseFloat(gameConfig.campaignBanner.offsetTop))}%`;
+
+    // Assert responsiveness
+    expect(campaignBanner).toHaveStyle({
+      top: expectedTop,
+    });
+
   });
 });
--- src/components/game-box/GameBox.test.tsx
+++ src/components/game-box/GameBox.test.tsx
@@ -1,15 +1,62 @@
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { GameBox } from './GameBox';
+import { gameConfig } from '../../config/game-config';
+import { TestElement } from '../../enums/TestElement';
 afterEach(cleanup);
 
 describe('[GAME] GameBox', () => {
   it('SHOULD Render GameBox with width maxHeight and maxWidth WHEN browsers viewport width and height is 1000px', () => {
-    //
+     // Mocking window.innerWidth and window.innerHeight to simulate a viewport size of 1000px x 1000px
+     global.innerWidth = 1000;
+     global.innerHeight = 1000;
+ 
+     const { container } = render(<GameBox>Content</GameBox>);
+ 
+     // Calculate expected dimensions based on the proportional design rules
+     const expectedWidth = gameConfig.container.maxWidth; // Original maxWidth
+     const expectedHeight = gameConfig.container.maxHeight;; // Original maxHeight
+ 
+     // Assert styles based on the updated gameConfig for the ContainerStyled component
+     const containerStyled = container.firstChild;
+ 
+     // Assert specific styles using toHaveStyle
+    expect(containerStyled).toHaveStyle('width: ' + `${expectedWidth}px`);
+    expect(containerStyled).toHaveStyle('height: ' + `${expectedHeight}px`);
+    expect(containerStyled).toHaveStyle('max-width: ' + `${expectedWidth}px`);
+    expect(containerStyled).toHaveStyle('max-height: ' + `${expectedHeight}px`);
+    expect(containerStyled).toHaveStyle(
+      'border-radius: ' + `${gameConfig.container.borderRadius}px`
+    );
+
+
+     // Assert data-testid attribute
+     expect(containerStyled).toHaveAttribute('data-testid', TestElement.GAME_BOX);
   });
 
   it('SHOULD Render GameBox with width 300px WHEN browsers viewport width 300px and height remains 1000px', () => {
     //
+  //  // Mocking window.innerWidth and window.innerHeight to simulate a viewport size of 300px x 1000px
+  //  global.innerWidth = 300;
+  //  global.innerHeight = 1000;
+
+  //  const { container } = render(<GameBox>Content</GameBox>);
+
+  //  // Calculate expected dimensions based on the proportional design rules
+  //  const expectedWidth = 300; // Fixed width for a viewport width of 300px
+
+  //  // Assert styles based on the updated gameConfig for the ContainerStyled component
+  //  const containerStyled = container.firstChild;
+
+  //  // Assert specific styles using toHaveStyle
+  //  expect(containerStyled).toHaveStyle('width: ' + `${expectedWidth}px`);
+  //  // Assuming other styles remain the same, you can add more assertions as needed
+
+  //  // Assert data-testid attribute
+  //  expect(containerStyled).toHaveAttribute('data-testid', TestElement.GAME_BOX);
+  
   });
 
   it('SHOULD Render GameBox with height 600px WHEN browsers viewport width 1000px and height is 600px', () => {
